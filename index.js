5a6d085ce1dfeb6ad346b0129b2d9d9ac1b18893
Soytemo7 <contador.manuel0@gmail.com>
2025-06-26 17:31:25 -0600

    Final

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ import redirectRouter from "./routes/redirect.route.js";
 const app = express(); 
 const PORT = process.env.PORT || 3000;
 
-const whiteList =[process.env.ORIGIN1];
+const whiteList =[process.env.ORIGIN1,process.env.ORIGIN2];
 
 app.use(cors({
     origin:function(origin,callback){
-        if(whiteList.includes(origin)){
+        if(!origin  || whiteList.includes(origin)){
             return callback(null,origin);
         }
         return callback("Error de CORS"+origin+" No autorizado");
